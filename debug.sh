15c8d45a662a69810ecce4265cacf7e48f43d1db
Mara Furland <mara@fur.land>
2025-10-31 15:46:43 -0400

    debug and flash script args

--- debug.sh
+++ debug.sh
@@ -4,32 +4,57 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 binary="$DIR/build/projectThing.elf"
 
+# shellcheck disable=SC2120
+h () {
+    # if arguments, print them
+    [ $# == 0 ] || echo "$*"
 
-# https://unix.stackexchange.com/a/358101/60480
-# cross-platform!
-#port=$( netstat -an | awk '
-#  $6 == "LISTEN" {
-#    if ($4 ~ "[.:][0-9]+$") {
-#      split($4, a, /[:.]/);
-#      port = a[length(a)];
-#      p[port] = 1
-#    }
-#  }
-#  END {
-#    for (i = 3000; i < 65000 && p[i]; i++){};
-#    if (i == 65000) {exit 1};
-#    print i
-#  }
-#')
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [OPTION]...
+  run gdb
+Available options:
+  -h, --help        display this help and exit
+  -m, --multicore   run multicore, bewarned that time won't pass unless you connect to the gdb port
+EOF
 
+    # if args, exit 1 else exit 0
+    [ $# == 0 ] || exit 1
+    exit 0
+}
 
-# multicore means multiple gdb per core
-# this runs gdb over a pipe for core 0, and puts conre 1 on port 3334
-#gdb-multiarch --quiet -ex "target extended-remote | openocd -c 'gdb_port pipe' -f interface/cmsis-dap.cfg -c'transport select swd' -c 'adapter speed 5000' -f target/rp2040.cfg -c 'rp2040.core1 configure -gdb-port 3334'"  -x gdbConfig "$binary"
+die() {
+    local msg=$1
+    local code=${2-1} # default exit status 1
+    echo >&2 -e  "$msg"
+    exit "$code"
+}
 
+# getopt short options go together, long options have commas
+TEMP=$(getopt -o hm --long help,multicore -n "$0" -- "$@")
+#shellcheck disable=SC2181
+if [ $? != 0 ] ; then
+    die "something wrong with getopt"
+fi
+eval set -- "$TEMP"
 
-# https://github.com/raspberrypi/debugprobe/issues/45#issuecomment-1332610961
-# The default configuration for openocd 0.12rc2 with an rp2040 target will debug both cores with dedicated gdb instances.
-# ...
-# The rp2040 timer is set up by default to stop counting when any core is stopped by a debugger and so the timer would never advance.
-gdb-multiarch --quiet -ex "target extended-remote | openocd -c 'gdb_port pipe' -f interface/cmsis-dap.cfg -c'transport select swd' -c 'adapter speed 5000' -c 'set USE_CORE 0' -f target/rp2040.cfg"  -x gdbConfig "$binary"
+multicore=false
+while true ; do
+    case "$1" in
+        -h|--help) h ;;
+        -m|--multicore) multicore=true; shift ;;
+        --) shift ; break ;;
+        *) die "issue parsing args, unexpected argument '$1'!" ;;
+    esac
+done
+
+if [[ "$multicore" == true ]]; then
+    # multicore means multiple gdb per core
+    # this runs gdb over a pipe for core 0, and puts conre 1 on port 3334
+    gdb-multiarch --quiet -ex "target extended-remote | openocd -c 'gdb_port pipe' -f interface/cmsis-dap.cfg -c'transport select swd' -c 'adapter speed 5000' -f target/rp2040.cfg -c 'rp2040.core1 configure -gdb-port 3334'"  -x gdbConfig "$binary"
+else
+    # https://github.com/raspberrypi/debugprobe/issues/45#issuecomment-1332610961
+    # The default configuration for openocd 0.12rc2 with an rp2040 target will debug both cores with dedicated gdb instances.
+    # ...
+    # The rp2040 timer is set up by default to stop counting when any core is stopped by a debugger and so the timer would never advance.
+    gdb-multiarch --quiet -ex "target extended-remote | openocd -c 'gdb_port pipe' -f interface/cmsis-dap.cfg -c'transport select swd' -c 'adapter speed 5000' -c 'set USE_CORE 0' -f target/rp2040.cfg"  -x gdbConfig "$binary"
+fi
